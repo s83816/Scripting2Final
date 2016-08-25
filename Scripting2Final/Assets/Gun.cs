d0b107bdeacd921fac1ede8428d58e632006b6d9
Dylan <dylmm@hotmail.com>
2016-08-24 21:45:55 -0700

    blah
    blahblah

--- Scripting2Final/Assets/Gun.cs
+++ Scripting2Final/Assets/Gun.cs
@@ -1,41 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Gun : MonoBehaviour {
+public class Gun : MonoBehaviour
+{
     public Vector3 WorldMouse;
 
     public int turnspeed;
 
     public GameObject ammo;
-    public float speed;
-    
-	// Use this for initialization
-	void Start () {
-        
+
+    // Use this for initialization
+    void Start()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
         aim();
-        
+
     }
-    
+
     void aim()
     {
         WorldMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Debug.Log(WorldMouse);
-            
-            WorldMouse.z = transform.position.z;
-            transform.LookAt(WorldMouse);
-           
-           
 
-            if (Input.GetMouseButtonDown(0))
-            {
-             Instantiate(ammo, transform.position,  transform.rotation);   
-            }
+        WorldMouse.z = transform.position.z;
+        transform.LookAt(WorldMouse);
+        //transform.LookAt(Input.mousePosition);
 
 
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(ammo, transform.position, transform.rotation);
         }
+
+
+    }
 }
---
+++ Scripting2Final/Assets/PlatformBullets.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addforce : MonoBehaviour {
+    Rigidbody rb;
+    public GameObject platform;
+    public float bulletspeed;
+    public float slowdown;
+
+    public bool withtime;
+    public float stoptime;
+
+    public bool isstopped;
+
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (isstopped == true && Input.GetMouseButtonDown(1))
+        {
+            Instantiate(platform,transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        if (withtime == false && bulletspeed > 0)
+        {
+            bulletspeed -= slowdown;
+            rb.velocity = transform.forward * bulletspeed;
+        }
+
+        if(bulletspeed <= 0)
+        {
+            bulletspeed = 0;
+            isstopped = true;
+
+            if (withtime == false)
+                rb.velocity = transform.forward * bulletspeed;
+        }
+        
+
+        if (withtime == true && stoptime > 0)
+        {
+            stoptime -= Time.deltaTime;
+            rb.velocity = transform.forward * stoptime;
+        }
+        if(stoptime<=0)
+        {
+            stoptime = 0;
+            isstopped = true;
+
+            if (withtime == true)
+                rb.velocity = transform.forward * stoptime;
+        }
+	}
+}
